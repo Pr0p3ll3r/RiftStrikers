5f524c1464d96f600f611eb491008ed928eed2e2
Jakub Dudek <kuba395@gmail.com>
2023-04-02 02:54:57 +0200

    Added Player movement and animations

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 offset;
+
+    public Transform player;
+
+    public void SetPlayer(Transform _player)
+    {
+        player = _player;
+    }
+
+    private void LateUpdate()
+    {
+        if(player != null)
+            transform.position = player.position + offset;
+    }
+}
