e95d30eba1ccbf932b833c28bb076b1f8cdd8f15
Jakub Dudek <kuba395@gmail.com>
2023-12-19 18:52:30 +0100

    Add pause menu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    private bool paused = false;
+
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject settingsMenu;
+
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button settingsButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private Button returnButton;
+
+    private void Start()
+    {
+        resumeButton.onClick.AddListener(() => {
+            TooglePause();
+        });
+        settingsButton.onClick.AddListener(() => {
+            Settings();
+        });
+        exitButton.onClick.AddListener(() => {
+            ReturnToMenu();
+        });
+        returnButton.onClick.AddListener(() => {
+            Return();
+        });
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            TooglePause();
+        }
+    }
+
+    private void TooglePause()
+    {
+        paused = !paused;
+   
+        transform.GetChild(0).gameObject.SetActive(paused);
+        pauseMenu.SetActive(paused);
+        settingsMenu.SetActive(false);
+    }
+
+    private void Settings()
+    {
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    private void Return()
+    {
+        transform.GetChild(0).gameObject.SetActive(paused);
+        pauseMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+    }
+
+    private void ReturnToMenu()
+    {
+        TooglePause();
+        LobbyManager.Instance.LeaveLobby();
+    }
+}
